25930ee950f1f5cd2b93b8f40ebbeb6b9cd0a504
Khoa Nguyen Dang Tran <khoandtran@ilavietnam.com>
2017-01-17 14:25:36 +0700

    fix hover li bg

--- OSC-Summer/js/customScript.js
+++ OSC-Summer/js/customScript.js
@@ -36,9 +36,4 @@ $(document).ready(function () {
         autoplay: false,
         autoplayDisableOnInteraction: false
     });
-//    $('.topmenu ul.nav li.dropdown').hover(function () {
-//        $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(100);
-//        }, function () {
-//            $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut(100);
-//        });
 });
